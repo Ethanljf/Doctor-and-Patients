c05366d08d738ec8e4a7d6c6d0a57de017f4e032
Ethan Foss <fossliam@outlook.com>
2018-11-07 18:48:51 -0400

    V3.0
    Overhauled functionality and amended problem surrounding incorrect memory allocation for structures.

--- A3.c
+++ A3.c
@@ -15,15 +15,15 @@ static void dequeue(void);
 static int queueSize(void);
 static int randTime(void);
 
-sem_t lock;
-sem_t emptyChairs;
 sem_t waiting;
-sem_t inExam;
-sem_t walkIn;
 sem_t doneExam;
-sem_t walkOut;
+sem_t inExam;
+pthread_mutex_t reception = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t exam = PTHREAD_MUTEX_INITIALIZER;
 
-int queue[4];
+int queue[4] = {0,0,0,0};
+int head = 0;
+int tail = 0;
 int goneHome;
 int patientCount;
 int inRoom = 0;
@@ -34,6 +34,7 @@ typedef struct{
 
 int main(void){
 
+    //Retrieve user input (desired number of patients)
     int patientNum;
     char userInput[81];
     char *inputTok;
@@ -45,47 +46,36 @@ int main(void){
     patientNum = atoi(inputTok);
     patientCount = patientNum;
 
+    //Create the doctor thread along with the desired number of patient threads
     pthread_t docThread, patThread[patientNum];
 
     //add error checks (-1 if failed)
-    sem_init(&lock, 1, 1);
-    sem_init(&emptyChairs, 1, 4);
     sem_init(&waiting, 1, 0);
-    sem_init(&inExam, 1, 0);
-    sem_init(&walkIn, 1, 1);
     sem_init(&doneExam, 1, 0);
-    sem_init(&walkOut, 1, 1);
-
-    for(int i=0; i<4; i++){
-        queue[i] = 0;
-    }
+    sem_init(&inExam, 1, 0);
 
     pthread_create(&docThread, NULL, doctor, NULL);
 
-    patientData **data = malloc(sizeof(patientData**));
+    patientData *data; 
+    data = malloc(4*sizeof(patientData));
 
     for(int i=0; i<patientNum; i++){
-        data[i] = malloc(sizeof(patientData));
-        data[i]->id = i+1;
-        sem_wait(&walkIn);
-        pthread_create(&patThread[i], NULL, patient, data[i]);
+        data->id = i+1;
+        pthread_create(&patThread[i], NULL, patient, data);
+        data++;
     }
 
     for(int i=0; i<patientNum; i++){
         pthread_join(patThread[i], NULL);
-        free(data[i]);
     }
 
+    free(data);
     pthread_join(docThread, NULL);
-    sem_destroy(&lock);
-    sem_destroy(&emptyChairs);
-    sem_destroy(&walkIn);
-    sem_destroy(&walkOut);
-    sem_destroy(&inExam);
-    sem_destroy(&doneExam);
     sem_destroy(&waiting);
+    sem_destroy(&doneExam);
+    sem_destroy(&inExam);
 
-    printf("All patients have been cured.\n");
+    printf("All patients have been examined.\n");
 
     return 0;
 }
@@ -99,15 +89,11 @@ void *doctor(void *param){
             printf("The Doctor is sleeping\n");
         }
         sem_wait(&waiting);
-        sem_wait(&lock);
-        sem_wait(&walkOut);
         //Critical section for doctor thread
         int x = randTime();
         printf("The Doctor is examining Patient %d for %d seconds. Chairs occupied = %d\n", queue[0], x, queueSize()-1);
         dequeue();
         sem_post(&inExam);
-        sem_post(&emptyChairs);
-        sem_post(&lock);
         sleep(x);
         sem_post(&doneExam);
     }
@@ -118,44 +104,48 @@ void *patient(void *param){
     int first = 0;
     int lab = 0;
     while(1){
-        sem_wait(&emptyChairs);
-        sem_wait(&lock);
+        reception:
+        pthread_mutex_lock(&reception);
         if(queueSize() == 4){
             int x = randTime();
             printf("\tWaiting room full. Patient %d drinking coffee for %d seconds\n", data->id, x);
+            pthread_mutex_unlock(&reception);
             sleep(x);
-        }
-        if(queueSize() == 0 && inRoom == 0){
+            goto reception;
+        } else if(queueSize() == 0 && inRoom == 0){
             printf("Waiting room is empty. Patient %d wakes up the doctor.\n", data->id);
-        }
-        if((queueSize() > 0 && queueSize() < 4) || (queueSize() == 0 && inRoom != 0)){
+            enqueue(data->id);
+            sem_post(&waiting);
+        } else if((queueSize() > 0 && queueSize() < 4) || (queueSize() == 0 && inRoom != 0)){
             printf("\tPatient %d is waiting. Chairs occupied = %d\n", data->id, queueSize()+1);
+            enqueue(data->id);
+            pthread_mutex_unlock(&reception);
         }
-        enqueue(data->id);  
-        sem_post(&walkIn);
-        sem_post(&waiting); 
-        sem_post(&lock);
 
         sem_wait(&inExam);
+        pthread_mutex_lock(&exam);
         if(first == 0){
             printf("Patient %d getting first examination\n", data->id);
             inRoom = data->id;
-            first = 1;
+            first++;
+            pthread_mutex_unlock(&reception);
             sem_wait(&doneExam);
-            inRoom = 0;
             int x = randTime();
             printf("Patient %d is visiting the lab for %d seconds\n", data->id, x);
-            sem_post(&walkOut);
+            inRoom = 0;
+            sem_post(&waiting);
+            pthread_mutex_unlock(&exam);
             sleep(x);
-            lab = 1;
+            lab++;
         } else if (lab == 1) {
             printf("Patient %d getting second examination\n", data->id);
-            goneHome++;
             inRoom = data->id;
+            goneHome++;
             sem_wait(&doneExam);
-            inRoom = 0;
             printf("Patient %d is going home\n", data->id);
-            sem_post(&walkOut);
+            inRoom = 0;
+            sem_post(&waiting);
+            pthread_mutex_unlock(&exam);
             pthread_exit(NULL);
         } 
     }
